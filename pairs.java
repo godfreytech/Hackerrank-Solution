a5a63c435bceb71954ee1920e5b03d96c72865b6
Godfrey Muiruri <46915987+godfreytech@users.noreply.github.com>
2020-01-16 16:42:55 +0300

    Add files via upload

---
+++ pairs.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception  {
+
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner inr = new Scanner (System.in);
+        int n = inr.nextInt();
+        int k = inr.nextInt();
+        int num[] = new int[n];
+        for(int i=0;i<n;i++){
+            num[i] = inr.nextInt();
+        }
+        Arrays.sort(num);
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(num[j] - num[i] == k || num[i] - num[j] == k){
+                    count++;
+                }
+                else if(num[j] - num[i] > k || num[i] - num[j] > k){
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+}
